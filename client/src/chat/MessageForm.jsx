a3a7f1eb96b970bf6485973dbbdaed82a26d00e4
Christian Stirling <christianstirling@outlook.com>
2026-02-25 10:59:04 -0600

    add 'thumbs up button for chat convenience

--- client/src/chat/MessageForm.jsx
+++ client/src/chat/MessageForm.jsx
@@ -10,18 +10,32 @@ export default function MessageForm({ onSend }) {
     setValue("");
   };
 
+  const handleThumbsUp = (e) => {
+    e.preventDefault();
+    onSend("👍");
+  };
+
   return (
-    <form action="" className="MessageForm" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="message_input"
-        placeholder="write a message.."
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <button type="submit" className="message_submit">
-        Send
-      </button>
-    </form>
+    <div className="MessageForm">
+      <form action="" className="form-left" onSubmit={handleThumbsUp}>
+        <button type="submit" className="message_submit">
+          👍
+        </button>
+      </form>
+
+      <form action="" className="form-right" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="message_input"
+          placeholder="write a message.."
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+
+        <button type="submit" className="message_submit">
+          Send
+        </button>
+      </form>
+    </div>
   );
 }
